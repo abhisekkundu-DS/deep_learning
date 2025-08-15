bdf4255e115060c6c1f5646848d89cd485ce0c3e
ABHISEK KUNDU <abhisekkundu86@gmail.com>
2025-08-15 15:56:05 +0530

    Create graident_desient_part2.py

---
+++ graident_desient_part2.py
@@ -0,0 +1,50 @@
+import torch
+
+# f = w * x
+#f = 2 * x
+
+X = torch.tensor([1,2,3,4], dtype=torch.float32)
+Y = torch.tensor([2,4,6,8], dtype=torch.float32)
+
+w = torch.tensor(0, dtype= torch.float32, requires_grad = True)
+#model predict
+def forward(x):
+  return w*x
+
+#loss = MSE
+def  loss(y, y_predict):
+  return ((y_predict - y)**2).mean()
+
+# gradient
+#MSE = 1/N * (w*x -y)
+# dj/dw = 1/N 2x (w*x - y)
+
+def gradient(x,y, y_predict):
+  return np.dot(2*x, y_predict -y).mean()
+
+print(f'prediction before traning : f(5) = {forward(5)}:.3f}')
+
+#Traning 
+learning_rate = 0.01
+n_iters = 10
+
+for epoch in range(n_iters):
+  #prediction = forward pass
+  y_pred = forward(X)
+  #loss
+  l = loss(Y, y_pred)
+  # gradients
+  # dw = gradient(X,Y,y_pred)
+  l.backward()
+  #updated_ weight
+  # w-= learning_rate * dw
+  with torch.no_grad():
+    w -= learning_rate * w.grad
+    
+
+if epoch % 1 == 0:
+  print(f'epoch {epoch + 1 }: w = {w: 3f} , loss = {l:.8f}')
+
+print(f'prediction before traning : f(5) = {forward(5)}:.3f}')
+  
+  
